47091874c33e14b4d5e7c72f87573873d5c898b2
MaestroVoodo <mykit@internet.ru>
2024-05-30 05:43:12 +0400

    Revert back sesion scope

--- src/main/java/org/app/bot/telegram/session/Session.java
+++ src/main/java/org/app/bot/telegram/session/Session.java
@@ -2,12 +2,10 @@ package org.app.bot.telegram.session;
 
 import lombok.Data;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.annotation.SessionScope;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 @Data
 @Component
-@SessionScope
 public class Session {
 
     private String lastButtonClicked;
